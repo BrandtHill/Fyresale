d584f0d02d79a8cadb0be22dfe22cabcc4ac5154
Brandt Hill <brandt.hill@mixonhill.com>
2019-08-14 12:37:01 -0500

    Use custom user agent (Amazon allows any)

--- lib/mix/tasks/get_html.ex
+++ lib/mix/tasks/get_html.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.GetHtml do
   use Mix.Task
   
   @headers [
-    {"User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"}
+    {"User-Agent", "Fyresale App (Elixir HTTPoison)"}
   ]
 
   @shortdoc "Runs Floki.find/2 on html body from url with selector"
--- lib/pricefinder.ex
+++ lib/pricefinder.ex
@@ -8,7 +8,7 @@ defmodule Fyresale.PriceFinder do
   alias Fyresale.{Product, ProductStore, Mailer}
 
   @headers [
-    {"User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"}
+    {"User-Agent", "Fyresale App (Elixir HTTPoison)"}
   ]
 
   @currency_regex ~r|[$¢£€¥₿]|
